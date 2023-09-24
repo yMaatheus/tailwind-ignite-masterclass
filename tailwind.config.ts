73a229bc0194368178818da8302459a549e3ff0e
Matheus <ymatheus.h@gmail.com>
2023-09-24 18:50:14 -0300

    chore: :wrench: add extend tailwind configuration grid template columns and max witdh

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,17 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: 'minmax(18rem, 20rem) 1fr',
+      },
+      maxWidth: {
+        app: '700px',
+      },
       colors: {
-        'diego': '#8257e6',
+        diego: '#8257e6',
       },
     },
   },
