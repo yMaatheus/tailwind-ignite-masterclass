dd686ba4504ccca388caab6416dbc4cdc80c167d
Matheus <ymatheus.h@gmail.com>
2023-09-24 18:27:01 -0300

    build: :heavy_plus_sign: setup prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1 className="flex">Hello world</h1>
-      <h2>Hello tailwind</h2>
-      <p>Hello worldinho</p>
-    </div>
-  )
+  return <h1>Hello world</h1>
 }
