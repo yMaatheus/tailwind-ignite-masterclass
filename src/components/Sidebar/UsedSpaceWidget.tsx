4e63af5b703c46e0b4fcc456afe7e7d93aa84037
Matheus <ymatheus.h@gmail.com>
2023-10-03 23:52:42 -0300

    fix: :bug: adjust progress bar to sidebar used space widget

--- src/components/Sidebar/UsedSpaceWidget.tsx
+++ src/components/Sidebar/UsedSpaceWidget.tsx
@@ -5,13 +5,13 @@ export function UsedSpaceWidget() {
         <span className="text-sm/5 font-medium text-violet-700 dark:text-zinc-100">
           Used space
         </span>
-        <p className="text-sm/5 text-violet-500 dark:text-zinc-400">
+        <span className="text-sm/5 text-violet-500 dark:text-zinc-400">
           Your team has used 80% of your available space. Need more?
-        </p>
+        </span>
       </div>
 
       <div className="h-2 rounded-full bg-violet-100 dark:bg-zinc-600">
-        <div className="h-2 w-4/5 rounded-full bg-violet-600 dark:text-zinc-400"></div>
+        <div className="h-2 w-4/5 rounded-full bg-violet-600 dark:bg-violet-400"></div>
       </div>
 
       <div className="space-x-3">
